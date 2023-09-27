dd1783a873a84f0da43d3b482e66a35064ff0d49
Netrunner <85088259+IUseDebianBtw@users.noreply.github.com>
2023-09-27 15:13:28 -0500

    Update tinyDfetch.d

--- src/tinyDfetch.d
+++ src/tinyDfetch.d
@@ -1,15 +1,12 @@
 import std.stdio;
-import std.conv;  // Import the required module for to!string
+import std.conv;
 import core.sys.posix.stdlib;
 
 void main() {
-    // Define a helper function to get environment variables
     string getEnv(const char* name) {
         auto result = getenv(name);
         return result ? to!string(result) : "";
     }
-
-    // Print the environment variables with colored output
     writeln("\x1b[31muser:\x1b[0m ", getEnv("USER"));
     writeln("\x1b[32msh:\x1b[0m ", getEnv("SHELL"));
     writeln("\x1b[33mterm:\x1b[0m ", getEnv("TERM"));
