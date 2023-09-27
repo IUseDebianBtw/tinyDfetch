aca1d5076141de09fcd976441bce0105ff9dd41d
Netrunner <85088259+IUseDebianBtw@users.noreply.github.com>
2023-09-27 15:16:41 -0500

    Update tinyDfetch.d

--- src/tinyDfetch.d
+++ src/tinyDfetch.d
@@ -1,15 +1,11 @@
-import std.stdio;
-import std.conv;
-import core.sys.posix.stdlib;
+import std.stdio   : writeln;
+import std.conv    : to;
+import std.process : environment;
 
 void main() {
-    string getEnv(const char* name) {
-        auto result = getenv(name);
-        return result ? to!string(result) : "";
-    }
-    writeln("\x1b[31muser:\x1b[0m ", getEnv("USER"));
-    writeln("\x1b[32msh:\x1b[0m ", getEnv("SHELL"));
-    writeln("\x1b[33mterm:\x1b[0m ", getEnv("TERM"));
-    writeln("\x1b[34mlocale:\x1b[0m ", getEnv("LANG"));
+    writeln("\x1b[31muser:\x1b[0m ",   environment.get("USER"));
+    writeln("\x1b[32msh:\x1b[0m ",     environment.get("SHELL"));
+    writeln("\x1b[33mterm:\x1b[0m ",   environment.get("TERM"));
+    writeln("\x1b[34mlocale:\x1b[0m ", environment.get("LANG"));
     writeln("\x1b[35mcolors: \x1b[0m\x1b[41m \x1b[42m \x1b[43m \x1b[44m \x1b[45m \x1b[0m");
 }
